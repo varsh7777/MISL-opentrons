6d89772611a8622140c7af000800e56835b2868f
Sri Varshitha Pinnaka <102142453+varsh7777@users.noreply.github.com>
2023-02-08 17:45:30 -0800

    Update ampure bead wash.py

--- ampure bead wash.py
+++ ampure bead wash.py
@@ -36,21 +36,18 @@ def run(protocol: protocol_api.ProtocolContext):
 
     for i in range(num_of_columns):
         if (bead_aspirate_vol > 20):
-            # should i control from where within the tiprack
-            # it is picking stuff up? i assume opentrons keeps
-            # track of this for us but just in case
             pipette1.pick_up_tip()
-            pipette1.aspirate(bead_aspirate_vol, trough['A1'])
-            # the computation done for where within the mag plate i want to
-            # blow out should work in theory, but should be tested
+            pipette1.aspirate(bead_aspirate_vol, trough['A1'], rate=0.05)
+            protocol.delay(seconds=2)
             pipette1.blow_out(mag_plate['A' + str(i + 1)])
-            pipette1.mix(10, supern_vol * 2/3)
+            pipette1.mix(10, supern_vol * 2/3, mag_plate['A' + str(i + 1)])
             pipette1.drop_tip()
         else:
             pipette2.pick_up_tip()
-            pipette2.aspirate(bead_aspirate_vol, trough['A1'])
+            pipette2.aspirate(bead_aspirate_vol, trough['A1'], rate=0.05)
+            protocol.delay(seconds=2)
             pipette2.blow_out(mag_plate['A' + str(i + 1)])
-            pipette2.mix(10, supern_vol * 2/3)
+            pipette2.mix(10, supern_vol * 2/3, mag_plate['A' + str(i + 1)])
             pipette2.drop_tip()
 
     protocol.delay(minutes=incubation_time)
@@ -62,29 +59,35 @@ def run(protocol: protocol_api.ProtocolContext):
         if (supern_vol > 20):
             pipette1.pick_up_tip()
             pipette1.aspirate(supern_vol + 5, mag_plate['A' + str(i + 1)],
-                              rate=0.1)
+                              rate=0.05)
+            protocol.delay(seconds=2)
             pipette1.drop_tip()
         else:
             pipette2.pick_up_tip()
             pipette2.aspirate(supern_vol + 5, mag_plate['A' + str(i + 1)],
-                              rate=0.1)
+                              rate=0.05)
+            protocol.delay(seconds=2)
             pipette2.drop_tip()
 
     for i in range(2):
         for j in range(num_of_columns):
             if (etoh_wash_volume + 5 > 20):
                 pipette1.pick_up_tip()
-                pipette1.aspirate(etoh_wash_volume, trough['A3'])
+                pipette1.aspirate(etoh_wash_volume, trough['A3'], rate=0.1)
+                protocol.delay(seconds=2)
                 pipette1.blow_out(mag_plate['A' + str(j + 1)])
                 protocol.delay(seconds=30)
-                pipette1.aspirate(etoh_wash_volume + 5, rate=0.1)
+                pipette1.aspirate(etoh_wash_volume + 5, mag_plate['A' + str(j + 1)], rate=0.05)
+                protocol.delay(seconds=2)
                 pipette1.drop_tip()
             else:
                 pipette2.pick_up_tip()
-                pipette2.aspirate(etoh_wash_volume, trough['A3'])
+                pipette2.aspirate(etoh_wash_volume, trough['A3'], rate=0.1)
+                protocol.delay(seconds=2)
                 pipette2.blow_out(mag_plate['A' + str(j + 1)])
                 protocol.delay(seconds=30)
-                pipette2.aspirate(etoh_wash_volume + 5, rate=0.1)
+                pipette2.aspirate(etoh_wash_volume + 5, mag_plate['A' + str(j + 1)], rate=0.05)
+                protocol.delay(seconds=2)
                 pipette2.drop_tip()
 
     # i'm assuming that each column's iteration in the above block
@@ -98,15 +101,19 @@ def run(protocol: protocol_api.ProtocolContext):
     for i in range(num_of_columns):
         if ((final_sample_volume + 2.5) > 20):
             pipette1.pick_up_tip()
-            pipette1.aspirate(final_sample_volume + 2.5, trough['A5'])
+            pipette1.aspirate(final_sample_volume + 2.5, trough['A5'], rate=0.05)
+            protocol.delay(seconds=2)
             pipette1.blow_out(mag_plate['A' + str(i + 1)])
-            pipette1.mix(10, (final_sample_volume + 2.5)/2)
+            pipette1.mix(10, (final_sample_volume + 2.5)/1.5, mag_plate['A' + str(i + 1)])
+            protocol.delay(seconds=2)
             pipette1.drop_tip()
         else:
             pipette2.pick_up_tip()
-            pipette2.aspirate(final_sample_volume + 2.5, trough['A5'])
+            pipette2.aspirate(final_sample_volume + 2.5, trough['A5'], rate=0.05)
+            protocol.delay(seconds=2)
             pipette2.blow_out(mag_plate['A' + str(i + 1)])
-            pipette2.mix(10, (final_sample_volume + 2.5)/2)
+            pipette2.mix(10, (final_sample_volume + 2.5)/1.5, mag_plate['A' + str(i + 1)])
+            protocol.delay(seconds=2)
             pipette2.drop_tip()
 
     protocol.delay(minutes=incubation_time)
@@ -119,12 +126,17 @@ def run(protocol: protocol_api.ProtocolContext):
         if (supern_vol_fin > 20):
             pipette1.pick_up_tip()
             pipette1.aspirate(supern_vol_fin, mag_plate['A' + str(i + 1)],
-                              rate=0.1)
+                              rate=0.01)
+            protocol.delay(seconds=2)
             pipette1.blow_out(mag_plate['A' + str(i + num_of_columns + 1)])
             pipette1.drop_tip()
         else:
             pipette2.pick_up_tip()
             pipette2.aspirate(supern_vol_fin, mag_plate['A' + str(i + 1)],
-                              rate=0.1)
+                              rate=0.01)
+            protocol.delay(seconds=2)
             pipette2.blow_out(mag_plate['A' + str(i + num_of_columns + 1)])
             pipette2.drop_tip()
+
+
+    mag_mod.disengage()
